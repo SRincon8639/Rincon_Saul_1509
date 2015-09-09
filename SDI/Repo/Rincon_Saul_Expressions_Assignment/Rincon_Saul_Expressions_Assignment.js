ee8aa150e70d580fcb95b4e9c02a6be88bd29da0
SRincon <recon8639@fullsail.edu>
2015-09-09 15:55:26 -0700

    Variables
    Create the variables

--- SDI/Repo/Rincon_Saul_Expressions_Assignment/Rincon_Saul_Expressions_Assignment.js
+++ SDI/Repo/Rincon_Saul_Expressions_Assignment/Rincon_Saul_Expressions_Assignment.js
@@ -5,4 +5,11 @@ SDI
 09/10/2015
 */
 
-alert("is this page working?");
+//alert("is this page working?");
+
+//Variables
+
+var myFirearms=20;					//Declares the variables 
+var fiveMoreFirearms=myFirearms+5;	//If you buy 5 more firearms, you will have myFirearms plus 5 more
+var tenLessFirearms=myFirearms-10;	//If you sell 10 firearms, you will have myFirearms minus 10
+var divideAllFirearms=myFirearms/2;	//This divides 20 firearms divided by 2
