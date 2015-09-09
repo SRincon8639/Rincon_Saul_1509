427a20fb4d7f6f1093d059862840302e5d9353ba
SRincon <recon8639@fullsail.edu>
2015-09-09 16:09:46 -0700

    Math
    the calculations

--- SDI/Repo/Rincon_Saul_Expressions_Assignment/Rincon_Saul_Expressions_Assignment.js
+++ SDI/Repo/Rincon_Saul_Expressions_Assignment/Rincon_Saul_Expressions_Assignment.js
@@ -12,4 +12,11 @@ SDI
 var myFirearms=20;					//Declares the variables 
 var fiveMoreFirearms=myFirearms+5;	//If you buy 5 more firearms, you will have myFirearms plus 5 more
 var tenLessFirearms=myFirearms-10;	//If you sell 10 firearms, you will have myFirearms minus 10
+var divideAllFirearms=myFirearms/2;	//This divides 20 firearms divided by 2var eightLessFirearms=myFirearms-8;	//If you sell 8 firearms, you will have myFirearms minus 8
 var divideAllFirearms=myFirearms/2;	//This divides 20 firearms divided by 2
+
+alert("I have 20 firearms");
+myFirearms=prompt("Enter the number from the alert");
+fiveMoreFirearms=prompt("I have" + " " + myFirearms + " " + "now, but if I buy 5 more, I will have" + "?");
+eightLessFirearms=prompt("I have"+ " " + myFirearms + " " + "now, but if I sell 8, I will have" + "?");
+divideAllFirearms=prompt("I sold half, now I have ");
