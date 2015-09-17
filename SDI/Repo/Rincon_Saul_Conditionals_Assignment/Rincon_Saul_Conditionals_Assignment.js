50c6951f71e59bfb0b6cdba3f6356d8aaa0ea936
SRincon <recon8639@fullsail.edu>
2015-09-16 19:48:54 -0700

    Conditionals
    The following are the conditionals listed

--- SDI/Repo/Rincon_Saul_Conditionals_Assignment/Rincon_Saul_Conditionals_Assignment.js
+++ SDI/Repo/Rincon_Saul_Conditionals_Assignment/Rincon_Saul_Conditionals_Assignment.js
@@ -19,3 +19,26 @@ alert("You have 100 tools but need 300 to attend armorer training.");//This aler
 console.log("I currently have" + " " + armorerTools + " " + " tools to attend armorer training" + ".");//The following console logs below will be the math equation
 console.log("I need " + " "+ toolsRequired + " "+ "tools to attend the armorer training"+ ".");
 console.log("Since I am short on tools, I need" +" "+ (toolsRequired-toolsToBuy)+" "+ "more to meet the requirement" + ".");
+//Conditionals
+
+if(armorerTools>=toolsRequired){				//If 100 is greater than or equal to 300, you can go to the training
+	console.log("You can attend the training!");
+}
+
+else{
+	var emptyString=prompt("How much tools do you need to buy?"); //The user will add a number
+}
+if(emptyString>=toolsToBuy){				//This empty string will be greater than or equal to 200 from user input
+	console.log("You have enough tools!");
+}
+else{
+	prompt("You need to enter 200!!");		//If the user does not follow the aler and adds a different number, it will prompt you to add 200
+	if("">=200){
+		console.log("You can't go to the training, you don't have enough tools");  //If the user input is less than 200, you cant attend training
+	}
+
+else{
+	prompt("For the last time, enter 200"); //If the user refuses to enter 200, they will be reminded again
+}
+}
+
