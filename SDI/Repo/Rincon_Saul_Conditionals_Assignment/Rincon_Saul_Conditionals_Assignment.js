329d52bd97a88e9690712366ba38a1f7bb0a9b04
SRincon <recon8639@fullsail.edu>
2015-09-16 19:32:56 -0700

    Outputs
    Outputs with explainations

--- SDI/Repo/Rincon_Saul_Conditionals_Assignment/Rincon_Saul_Conditionals_Assignment.js
+++ SDI/Repo/Rincon_Saul_Conditionals_Assignment/Rincon_Saul_Conditionals_Assignment.js
@@ -5,4 +5,17 @@ Conditionals Assignment
 09/17/2015
 */
 
-alert("hi");
+//alert("hi");
+
+//Variables
+var armorerTools=100;
+var toolsRequired=300;
+var toolsToBuy=200;
+
+
+
+//Outputs
+alert("You have 100 tools but need 300 to attend armorer training.");//This alert will let user know the amounts of tools he has on hand at the moment
+console.log("I currently have" + " " + armorerTools + " " + " tools to attend armorer training" + ".");//The following console logs below will be the math equation
+console.log("I need " + " "+ toolsRequired + " "+ "tools to attend the armorer training"+ ".");
+console.log("Since I am short on tools, I need" +" "+ (toolsRequired-toolsToBuy)+" "+ "more to meet the requirement" + ".");
